82dceadbe4f79fc4a792cfcb2779a6b493870e4e
Scott Colson <scott@colsoncode.com>
2021-10-20 10:04:37 -0700

    Add argument extension to simplify working with input/output parameters.

---
+++ Sample.WorkflowActivity/SampleWorkflowActivity.cs
@@ -0,0 +1,27 @@
+﻿namespace Sample.WorkflowActivity
+{
+    using System.Activities;
+    using CCLLC.CDS.Sdk;
+    using Microsoft.Xrm.Sdk.Workflow;    
+
+    public class SimpleWorkflowActivity : CDSWorkflowActivity
+    {
+        [Input("data type input")]
+        public InArgument Input1 { get; set; }
+
+        [Output("data type output")]
+        public OutArgument Output1 { get; set; }
+
+        public override void ExecuteInternal(ICDSWorkflowExecutionContext executionContext)
+        {
+            // Get input
+            var input1 = Input1.Get<string>(executionContext);
+
+            // Do stuff 
+
+            // Set output
+            Output1.Set(executionContext, "value");
+           
+        }
+    }
+}
